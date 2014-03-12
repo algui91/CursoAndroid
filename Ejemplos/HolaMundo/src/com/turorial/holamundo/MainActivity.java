247904ea4debc2ca1e7f17d92d1c7344eeb0e219
Alejandro Alcalde <algui91@gmail.com>
2014-03-12 08:57:50 +0100

    Formating code

--- Ejemplos/HolaMundo/src/com/turorial/holamundo/MainActivity.java
+++ Ejemplos/HolaMundo/src/com/turorial/holamundo/MainActivity.java
@@ -6,29 +6,30 @@ import android.app.Activity;
 import android.view.Menu;
 
 /**
- * Clase principal, hereda de la clase Activity, por lo cual dispondrá de interfaz de usuario.
- * Al crear una Activity, Android invoca a una serie de métodos, entre ellos <i>oncreate()</i>. 
- * El ciclo de vida de una activity se puede ver en <a href="http://developer.android.com/reference/android/app/Activity.html#ActivityLifecycle" target="_blank">Activity Lifecycle</a>
+ * Clase principal, hereda de la clase Activity, por lo cual dispondrá de
+ * interfaz de usuario. Al crear una Activity, Android invoca a una serie de
+ * métodos, entre ellos <i>oncreate()</i>. El ciclo de vida de una activity se
+ * puede ver en <a href="http://developer.android.com/reference/android/app/Activity.html#ActivityLifecycle" target="_blank">Activity Lifecycle</a>
  * 
  * @see http://developer.android.com/reference/android/app/Activity.html#ActivityLifecycle
  * @author Alejandro Alcalde
- *
  */
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         /**
-         * Método encargado de “inflar” la actividad. Inicializar cada componente de la actividad 
-         * con su correspondiente View.
+         * Método encargado de “inflar” la actividad. Inicializar cada
+         * componente de la actividad con su correspondiente View.
          */
         setContentView(R.layout.activity_main);
     }
 
     /**
-     * Crea los menús definidos en los recursos, sólo se llama una vez, al pulsar el botón de menú.
+     * Crea los menús definidos en los recursos, sólo se llama una vez, al
+     * pulsar el botón de menú.
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
