1f8f5700edd48c6e7f9ad16e7748e812a1fd5d47
Alejandro Alcalde <algui91@gmail.com>
2014-03-14 08:09:13 +0100

    Added License header

--- Ejemplos/HolaMundo/src/com/turorial/holamundo/MainActivity.java
+++ Ejemplos/HolaMundo/src/com/turorial/holamundo/MainActivity.java
@@ -1,3 +1,17 @@
+/**
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package com.turorial.holamundo;
 
@@ -15,7 +29,7 @@ import android.widget.EditText;
  * métodos, entre ellos <i>oncreate()</i>. El ciclo de vida de una activity se
  * puede ver en <a href=
  * "http://developer.android.com/reference/android/app/Activity.html#ActivityLifecycle">Activity Lifecycle</a>
- * 
+ *
  * @see http://developer.android.com/reference/android/app/Activity.html#
  *      ActivityLifecycle
  * @author Alejandro Alcalde
