c90d17e6129764caaa756668887a6dcbff011db1
Alejandro Alcalde <algui91@gmail.com>
2015-04-09 23:41:02 +0200

    Integrating FragmentBasics into MainApp

--- Ejemplos/MainApp/app/src/main/java/es/tformacion/mainapp/MainActivity.java
+++ Ejemplos/MainApp/app/src/main/java/es/tformacion/mainapp/MainActivity.java
@@ -11,16 +11,19 @@ public class MainActivity extends MaterialNavigationDrawer {
 
     @Override
     public void init(Bundle bundle) {
-        MaterialSection helloWorld = newSection("HelloWorld", R.mipmap.ic_launcher,
+        MaterialSection helloWorld = newSection("Hello World", R.mipmap.ic_launcher,
                 HelloWorldMain.newInstance());
-        MaterialSection dataPers = newSection("DataPersistency", R.mipmap.ic_launcher,
+        MaterialSection dataPers = newSection("Data Persistency", R.mipmap.ic_launcher,
                 new Intent("es.tformacion.datapersistency.DataPersistencyMainActivity"));
-        MaterialSection basicAdap = newSection("BasicAdapter", R.mipmap.ic_launcher,
+        MaterialSection basicAdap = newSection("Basic Adapter", R.mipmap.ic_launcher,
                 new Intent("es.tformacion.basicadapter.data.ui.BasicAdapterMainActivity"));
+        MaterialSection fragmentBasics = newSection("Fragment Basics", R.mipmap.ic_launcher,
+                new Intent("es.tformacion.fragmentbasics.FragmentBasicsMainActivity"));
 
         addSection(helloWorld);
         addSection(dataPers);
         addSection(basicAdap);
+        addSection(fragmentBasics);
 
         setDefaultSectionLoaded(0);
     }
